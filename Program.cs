a5ec3c3580fe6adae1d2e081fcf607c141053852
13MAD86, Martin <rikkuff2000@yahoo.de>
2025-10-28 13:59:22 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Threading;
 
 #if APRSGATEWAY
+#if GUI
 using System.Windows.Forms;
 
 namespace APRSForwarder
@@ -22,14 +23,28 @@ namespace APRSForwarder
     }
 }
 #else
+namespace APRSForwarder
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+			APRSGateWay gateway = new APRSGateWay();
+            gateway.Start();
+            // gateway.Stop();
+        }
+    }
+}
+#endif
+#else
 namespace APRSWebServer
 {
     class Program
     {        
         static void Main(string[] args)
         {
-			APRSServer server = new APRSServer();                        
-            server.Start();            
+			APRSServer server = new APRSServer();
+            server.Start();
             Console.WriteLine("Type exit to Exit:");
             while (true) if(Console.ReadLine() == "exit") break;
             Console.WriteLine("exiting...");
